5a62ad3bd9128bd13d2d9a480cc0e5ce4330c038
rhel <rhel@rhel.imac.local>
2019-03-21 03:08:51 +0000

    phonenumber and email extractor created

--- automate_python/phoneNumberEmailExtractor.py
+++ automate_python/phoneNumberEmailExtractor.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+# page 165
 
 import re
 import sys
+
+memo = 'This is a document packedg with phone numbers such as 0201234567 \
+        and email address such as ginger@palmtree.com\
+        extra rubbish @ me sir.'
+
+def getEmail(doc):
+    getEmailRegex = re.compile(r'[a-z0-9\.\-\_]+\@[a-z0-9\.\-\_]+', re.IGNORECASE | re.VERBOSE | re.DOTALL)
+
+
+
+def getPhoneRegex(doc):
