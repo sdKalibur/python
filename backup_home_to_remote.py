c771a8bb1c10548c3d3b6433de77c766978b085b
kaliburx <kaliburx@gmail.com>
2020-12-01 12:55:39 +0000

    Created README.MD

--- backup_home_to_remote.py
+++ backup_home_to_remote.py
@@ -4,20 +4,26 @@ import os
 from multiprocessing import Pool
 
 user = "kalibur"
-dest = "/tmp/" + user  + "/"
-#dest = "/kal-net/backup/kal-inspiron/home/" + user  + "/"
-backup_host = 'localhost'
-#backup_host = 'kal-bunker'
+# dest = "/tmp/" + user  + "/"
+dest = "/kal-net/backup/kal-inspiron/home/" + user  + "/"
+#backup_host = 'localhost'
+backup_host = 'kal-bunker'
 user_home = "/home/" + user + '/'
 
 
 def backup_job(item):
     """ Spins backups threads """
     print("Backing up : {} ".format(item))
-    subprocess.run(["rsync","-avz", user_home + item," " , backup_host+':'+dest])
+    subprocess.run(["echo","rsync","-arihvv", user_home + item," " , backup_host+':'+dest])
 
-# src = "/data/prod/"
-# src = "/tmp/ktest/"
+def check_free_space():
+    """checks if there is enought space on remote system"""
+    subprocess.run(["ssh",backup_host, "du -h", dest ]])
+    # get output
+
+def used_space():
+    pass
+
 
 if __name__ == "__main__":
     """setup direction varaialbes and call multithreading Pools."""
@@ -29,6 +35,12 @@ if __name__ == "__main__":
     p = Pool(len(targets))
     print(p)
     # Start each task within the pool
+
+    free_space = check_free_space()
+    used_space = Check_required_space()
+    
+    if 
+
     try:
         p.map( backup_job, targets)
     except Exception as e:
