062f7387936e67eaa23fbe4d74f94b185105bf89
kaliburx <kaliburx@gmail.com>
2020-12-09 00:51:06 +0200

    put trailing / to post url and stripped \n

--- coursera/django_dicts/readfile_lines_2_dict.py
+++ coursera/django_dicts/readfile_lines_2_dict.py
@@ -3,7 +3,7 @@
 import os, requests
 
 feedback_dir = '/data/feedback'
-url = 'http://34.67.208.199/feedback'
+url = 'http://35.223.29.59/feedback/'
 #print(os.listdir(feedback_dir))
 
 feedback_files = os.listdir('/data/feedback') 
@@ -13,10 +13,14 @@ for file in feedback_files :
     with open( feedback_dir + '/' + file, 'r') as f:    
         #print(f.readlines())
         fd = f.readlines()
-        all_feedback.append({'title':fd[0], 'name':fd[1], 'date':fd[2], 'feeback':fd[3]})
+        #print(fd)
+        all_feedback.append({'title':fd[0].strip(), 'name':fd[1].strip(), 'date':fd[2].strip(), 'feedback':fd[3].strip()})
     f.close()
 
-print(all_feedback)
+#print(all_feedback)
 
-request.post(url, all_feedback)
-
+for i in all_feedback:
+    print('Will post : {}'.format(i))
+    post = requests.post(url, data = i)
+    print(post.status_code)
+#requests.post(url, all_feedback)
